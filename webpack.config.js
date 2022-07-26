07fb36ccefcc33aed38d07bad967fc6446eb2cd1
Clara Smyth <clarasmyth.work@gmail.com>
2022-07-27 00:22:02 +0100

    Setup eslint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -32,6 +33,6 @@ module.exports = {
         }),
     ],
     optimization: {
-        minimizer: [new CssMinimizerPlugin()],
+        minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
     },
 };
