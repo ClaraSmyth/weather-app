c76cdad30b8bb10da872b218fafaf9cc829c1353
Clara Smyth <clarasmyth.work@gmail.com>
2022-07-30 12:30:43 +0100

    Added some additional elements and styling

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,17 +1,26 @@
-import format from 'date-fns/format';
+import { format, getUnixTime, fromUnixTime, formatISO } from 'date-fns';
 
 const renderInfo = (weather) => {
     const location = document.querySelector('.card-location');
     const date = document.querySelector('.card-date');
     const time = document.querySelector('.card-time');
     const temp = document.querySelector('.card-temp');
+    const desc = document.querySelector('.card-desc');
+    const icon = document.querySelector('.card-icon');
 
-    const [currentDate, currentTime] = format(new Date(), 'do MMM y~p').split('~');
+    const unixTime = getUnixTime(new Date());
+    const unixToUtc = fromUnixTime(unixTime + weather.timezone).toUTCString();
+    console.log(new Date());
+    console.log(new Date(unixToUtc));
 
-    location.innerText = weather.name;
+    const [currentDate, currentTime] = format(fromUnixTime(unixTime), 'do MMM R~p').split('~');
+
+    location.innerText = `${weather.name}, ${weather.sys.country}`;
     date.innerText = currentDate;
     time.innerText = currentTime;
     temp.innerText = weather.main.temp;
+    desc.innerText = weather.weather[0].description;
+    icon.src = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
 };
 
 export default renderInfo;
