9df17833b573cc03c1dbeb07b2c4585216448719
Clara Smyth <clarasmyth.work@gmail.com>
2022-08-01 02:40:13 +0100

    Added event listener for grabbing user input

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import getWeather from './modules/api';
 import './styles/styles.scss';
 
+const form = document.querySelector('.card-search');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userInput = document.querySelector('.card-search-input');
+    getWeather(userInput.value);
+    userInput.value = '';
+});
+
 getWeather('London');
