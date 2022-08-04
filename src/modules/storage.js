174fb548cc06c7973b4fa5221898dcde96ac7b3e
Clara Smyth <clarasmyth.work@gmail.com>
2022-08-04 02:35:18 +0100

    Added storage module with filler content for now

---
+++ src/modules/storage.js
@@ -0,0 +1,12 @@
+const storedLocations = [
+    { name: 'London, GB', lat: 51.5074, lon: -0.1278 },
+    { name: 'Madrid City Center, ES', lat: 40.4167, lon: -3.7036 },
+    { name: 'Québec, CA', lat: 46.8137, lon: -71.2084 },
+    { name: 'Kyoto, JP', lat: 35.021, lon: 135.7556 },
+    { name: 'New York, US', lat: 40.7127, lon: -74.006 },
+    { name: 'California, US', lat: 38.6287, lon: -92.566 },
+    { name: 'Los Angeles, US', lat: 34.0537, lon: -118.2428 },
+    { name: 'Moscow, RU', lat: 55.754, lon: 37.6204 },
+];
+
+export default storedLocations;
