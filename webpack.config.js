ff06d0f2494e51d612dd67c5af6e6b8a32d8482a
Clara Smyth <clarasmyth.work@gmail.com>
2022-10-06 07:33:55 +0100

    Added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
@@ -30,6 +31,9 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: './[name].[contenthash].css',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'src/assets', to: 'assets' }],
         }),
     ],
     optimization: {
